91866630389a909a5d172b1b4ae91e19495d37ae
clofd <clofd1996@gmail.com>
2022-12-02 13:22:32 +0800

    Increase Giant HP 3 to 4

--- Assets/Scripts/Giant.cs
+++ Assets/Scripts/Giant.cs
@@ -9,7 +9,8 @@ public class Giant : Enemy
         Idle = 0,
         Chasing = 1,
         Attacking = 2,
-        Berserk = 3
+        Injury = 3,
+        Berserk = 4
     }
 
     [SerializeField] GameObject knifePrefab;
@@ -51,14 +52,23 @@ public class Giant : Enemy
                 giantState = GiantState.Idle;
                 waitTimer = 5f;
                 break;
-            case GiantState.Berserk:
-                animator.SetTrigger("Attack");
-                waitTimer = 5f;
-                waitTimer -= Time.deltaTime;
-                if (waitTimer <= 0)
+            case GiantState.Injury:
+                base.Update();
+                float hpRatio = (float)player.playerHP / player.maxHP;
+                if (hpRatio < 0.5)
                 {
                     giantState = GiantState.Berserk;
                 }
+                break;
+            case GiantState.Berserk:
+                base.Update();
+                animator.SetTrigger("Attack");
+                //waitTimer = 5f;
+                //waitTimer -= Time.deltaTime;
+                //if (waitTimer <= 0)
+                //{
+                //    giantState = GiantState.Berserk;
+                //}
                 break;
             default:
                 break;
